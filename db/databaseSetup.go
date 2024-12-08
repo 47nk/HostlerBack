9a3f730bcaa43311d563cd19075bc7ee05674c44
47nk <nk578311@gmail.com>
2024-12-08 15:58:34 +0530

    password check add

--- db/databaseSetup.go
+++ db/databaseSetup.go
@@ -9,19 +9,27 @@ import (
 )
 
 func InitializeDB() (*gorm.DB, error) {
-	dsn := "postgresql://postgres.ixuwoesbfkzrwlzxtsic:Naresh@007@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := " REMOVED"
+	//open connection to db
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
 	}
 
 	sqlDB, err := db.DB()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to retrieve database instance: %w", err)
 	}
 
 	if err := sqlDB.Ping(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	err = db.Exec("DEALLOCATE ALL").Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to deallocate prepared statements: %w", err)
 	}
 
 	fmt.Println("Successfully connected to the PostgreSQL database!")
